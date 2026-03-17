2294a81e51d39b236d8e90dc260215c22d0da440
Umarr ‎ <umerqaiser983@gmail.com>
2026-03-17 09:05:53 +0500

    Revert "sett"
    This reverts commit ac2ee9cb5eb9c9606d7092b411fc2261fb8eda5e.

--- src/components/Settings.tsx
+++ src/components/Settings.tsx
@@ -412,7 +412,7 @@ export function Settings() {
 
       <div style={{ height: 4 }} />
       <p style={{ textAlign: 'center', fontSize: 11, fontWeight: 700, color: 'var(--muted)', paddingBottom: 8 }}>
-        Made for Ammu with ❤️ by Umo 
+        made by Umo with ❤️ for ammu
       </p>
     </div>
   )
